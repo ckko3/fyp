36a0f1d1d45dcd2b7f88161d506cbcaa3c7f0262
ckko3 <ckko3@se.cuhk.edu.hk>
2016-01-28 17:42:05 +0800

    Page Created

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# fyp
+# Fake Your Professor
---
+++ script.js
@@ -0,0 +1,60 @@
+var link;
+
+function load() {
+  var min = 1;
+  var max = 15;
+  var num = Math.floor((Math.random() * max) + min);
+
+  switch (num) {
+    case 1:
+    link = "https://docs.google.com/forms/d/17wogmmjhO0zvnLWzhmLmDjioKe8Z4kCCcne8Vam26hI/viewform";
+    break;
+    case 2:
+    link = "https://docs.google.com/forms/d/17wogmmjhO0zvnLWzhmLmDjioKe8Z4kCCcne8Vam26hI/viewform";
+    break;
+    case 3:
+    link = "https://docs.google.com/forms/d/17wogmmjhO0zvnLWzhmLmDjioKe8Z4kCCcne8Vam26hI/viewform";
+    break;
+    case 4:
+    link = "https://docs.google.com/forms/d/17wogmmjhO0zvnLWzhmLmDjioKe8Z4kCCcne8Vam26hI/viewform";
+    break;
+    case 5:
+    link = "https://docs.google.com/forms/d/17wogmmjhO0zvnLWzhmLmDjioKe8Z4kCCcne8Vam26hI/viewform";
+    break;
+    case 6:
+    link = "https://docs.google.com/forms/d/17wogmmjhO0zvnLWzhmLmDjioKe8Z4kCCcne8Vam26hI/viewform";
+    break;
+    case 7:
+    link = "https://docs.google.com/forms/d/17wogmmjhO0zvnLWzhmLmDjioKe8Z4kCCcne8Vam26hI/viewform";
+    break;
+    case 8:
+    link = "https://docs.google.com/forms/d/17wogmmjhO0zvnLWzhmLmDjioKe8Z4kCCcne8Vam26hI/viewform";
+    break;
+    case 9:
+    link = "https://docs.google.com/forms/d/17wogmmjhO0zvnLWzhmLmDjioKe8Z4kCCcne8Vam26hI/viewform";
+    break;
+    case 10:
+    link = "https://docs.google.com/forms/d/17wogmmjhO0zvnLWzhmLmDjioKe8Z4kCCcne8Vam26hI/viewform";
+    break;
+    case 11:
+    link = "https://docs.google.com/forms/d/17wogmmjhO0zvnLWzhmLmDjioKe8Z4kCCcne8Vam26hI/viewform";
+    break;
+    case 12:
+    link = "https://docs.google.com/forms/d/17wogmmjhO0zvnLWzhmLmDjioKe8Z4kCCcne8Vam26hI/viewform";
+    break;
+    case 13:
+    link = "https://docs.google.com/forms/d/17wogmmjhO0zvnLWzhmLmDjioKe8Z4kCCcne8Vam26hI/viewform";
+    break;
+    case 14:
+    link = "https://docs.google.com/forms/d/17wogmmjhO0zvnLWzhmLmDjioKe8Z4kCCcne8Vam26hI/viewform";
+    break;
+    case 15:
+    link = "https://docs.google.com/forms/d/17wogmmjhO0zvnLWzhmLmDjioKe8Z4kCCcne8Vam26hI/viewform";
+    break;
+
+  }
+}
+
+function go() {
+  window.location = link;
+}
