f59dfc6a667f87e3dde3a23525b27c7eeb832812
ckko3 <ckko3@se.cuhk.edu.hk>
2016-02-06 17:47:30 +0800

    change to 100/set x 10set

--- script.js
+++ script.js
@@ -1,52 +1,37 @@
 $(document).ready(function() {
   var link;
-  var num = Math.floor((Math.random() * 15) + 1);
+  var num = Math.floor((Math.random() * 10) + 1);
 
   switch (num) {
     case 1:
-    link = "https://docs.google.com/forms/d/1qstPK3yP9plXbP-Pv0ZBYi-fLEb8tec_4IKx5wlkeo8/viewform";
+    link = "http://goo.gl/forms/0qwRe2sWDM";
     break;
     case 2:
-    link = "https://docs.google.com/forms/d/1SOEP51jhck203HWBGzxCq8pFYqriNPj0gPAjpiFZ6V8/viewform";
+    link = "http://goo.gl/forms/8LtkexZ9O4";
     break;
     case 3:
-    link = "https://docs.google.com/forms/d/1s_Bp0eVkkkISO-G9jwZz3dpIizorP_OLo8fNkALDN9Q/viewform";
+    link = "http://goo.gl/forms/VLIJosVAxY";
     break;
     case 4:
-    link = "https://docs.google.com/forms/d/122n3G4lO27y3Dj4nJTx2Zz7CDffeZV5GKZjk-mvwWv4/viewform";
+    link = "http://goo.gl/forms/xxqS5sw6RQ";
     break;
     case 5:
-    link = "https://docs.google.com/forms/d/1oBqiNkDwMVzz71TLXH0yW__kgxJ9c-srBc-D4ARrX4w/viewform";
+    link = "http://goo.gl/forms/blJwCsyCml";
     break;
     case 6:
-    link = "https://docs.google.com/forms/d/1jurlOQwhpC7WU0GQYs4oNr4ob39kh7Ngw_RwTvKT1mg/viewform";
+    link = "http://goo.gl/forms/EEYl9ao24n";
     break;
     case 7:
-    link = "https://docs.google.com/forms/d/1UGmXp4zrVd8lNGgTWeoA1l6GgTt5toWrEWoJNWP3Xyw/viewform";
+    link = "http://goo.gl/forms/0FQvf9i6Nm";
     break;
     case 8:
-    link = "https://docs.google.com/forms/d/1h0A6mUf5H-BRNAMxTQB4A3ZgHj46Vf4JqVF6uHDjxbM/viewform";
+    link = "http://goo.gl/forms/v6BdY4HE4K";
     break;
     case 9:
-    link = "https://docs.google.com/forms/d/1yTPRJzsZVRqn0LJEFElb_kVft7bTBxqSQM5pVi7XjJU/viewform";
+    link = "http://goo.gl/forms/oeL8oGdKMa";
     break;
     case 10:
-    link = "https://docs.google.com/forms/d/1VYVWhn8s3W0hNgxDZmXqeGKE3ybOZXr-ymJojfLL8kU/viewform";
-    break;
-    case 11:
-    link = "https://docs.google.com/forms/d/1w5DIWlopDS9J34syLk4pievCdZGDgKnS8h1Lu_PQNr4/viewform";
-    break;
-    case 12:
-    link = "https://docs.google.com/forms/d/1GygqnvYG7ixIz-CcHAqbEWYuGfdiO4vePqERAbBeqO8/viewform";
-    break;
-    case 13:
-    link = "https://docs.google.com/forms/d/1h0zj_UQX1O0kjnYbdmi9qbnhvPROITyvqTond7DxzP8/viewform";
-    break;
-    case 14:
-    link = "https://docs.google.com/forms/d/1Z1hViVbBcz5-dBwP9wcRjFWskaw924HdToazhcv4QtY/viewform";
-    break;
-    case 15:
-    link = "https://docs.google.com/forms/d/1VBhmQGSGli4QSDTrzVLaWC6IZp-wlPfrTMavrX8UxiQ/viewform";
+    link = "http://goo.gl/forms/W3qGzz1sbp";
     break;
   }
 
